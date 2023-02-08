7359f6404da854a30a6489ec4926437c8a3c7bcf
Adrian Benea <adrian.benea@bytex.ro>
2023-02-08 12:16:24 +0200

    Configured for deployment. :wq

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ysyn8q!ce6@gxat75=osblj57&sh()#0uj6#szr=@+v%1pq1+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['0.0.0.0',]
+ALLOWED_HOSTS = ['0.0.0.0',
 
 
 # Application definition
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
